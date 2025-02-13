47c0428592f0e6ad2fff4731d9c9963e8ea2dd68
osho sunkanmi harry <oshosunkanmi270@gmail.com>
2025-02-13 01:48:25 +0100

    Fix case sensitivity for component imports

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
     <div className='text-white'>
         <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center' >
             <p className='text-[#00df9a] font-bold p-2' >GROWING WITH DATA ANALYTICS</p> 
-            <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold' >Grow with data</h1>
+            <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold' >Grow with dataa</h1>
             <div className='flex justify-center items-center'>
                 <p className='md:text-5xl sm:text-4xl text-xl font-bold py-4' >Fast, flexible financing for</p>
                 <TypeAnimation
