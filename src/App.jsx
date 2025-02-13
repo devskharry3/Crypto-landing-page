e6072fdbe2fdec61c4ebc75ed9ca5c925055219c
osho sunkanmi harry <oshosunkanmi270@gmail.com>
2025-02-13 01:35:30 +0100

    Fix case sensitivity issue for Hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
-import Hero from './Components/hero'
+import Hero from './Components/Hero'
 import Analytics from './Components/Analytics'
 import NewsLetter from './Components/NewsLetter'
 import Cards from './Components/Cards'
